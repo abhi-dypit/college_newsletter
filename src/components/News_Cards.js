7309ae1f1674938d66fc4ff2e4cc76f4c4c99e97
K.Abhishek <abhishekgglacc@gmail.com>
2022-04-29 19:14:50 +0530

    updated basic data visualization

--- src/components/News_Cards.js
+++ src/components/News_Cards.js
@@ -1,30 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Row, Col, Card} from 'react-bootstrap';
 import logo from '../logo.svg';
+import News_Data from './News_Data.json';
 import './News_Cards.css';
 
 function News_Cards()
 {
     return(
         <div className='News'>
-        
+                {News_Data.map((data) => (
+            <>
         <Row xs={1} md={5} className="g-4">
   {Array.from({ length: 5 }).map((_, idx) => (
     <Col>
       <Card border="primary">
-        <Card.Img variant="top" src={logo} />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
-          </Card.Text>
-          <Button variant="primary">Read More</Button>
-        </Card.Body>
+        <>
+
+            <Card.Img variant="top" src={logo} />
+            <Card.Body>
+              <Card.Title>{data.title}</Card.Title>
+              <Card.Text>{data.content}</Card.Text>
+              <Card.Text>{data.docs_link}</Card.Text>
+              <Card.Text>{data.img_link}</Card.Text>
+              <Button variant="primary">Read More</Button>
+            </Card.Body>
+            </>
+
       </Card>
     </Col>
   ))}
-</Row>
+</Row> </>
+))}
         </div>
     )
 }
